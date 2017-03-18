c949c05c9cd79d5abd3066dbc230675859d8980c
ESI Design <pete@Peters-MacBook-Pro.local>
2017-03-17 21:20:55 -0400

    Westfield Ticket take 2
    Trying again…

--- wp-content/themes/esc_2016/index.php
+++ wp-content/themes/esc_2016/index.php
@@ -27,8 +27,7 @@
 		$count = 1;
 		while( have_rows('headers') ): the_row(); 
 		$header = get_sub_field('header'); 
-		//I think this will do what I want - PV
-		/*if($count == 1) {
+		if($count == 1) {
 			echo '<div><h4 class="westfield">';
 		} else if($count == 3) {
 			echo '<div><h4 class="blue">';	
@@ -37,10 +36,10 @@
 		}
 		echo $header.'</h4></div>';
 		if($count == 1) {
-			echo '<div><a href="'.get_field('tickets_link', 2).'" class="tickets" target="_blank">Tickets</a></div>';
+			//echo '<div><a href="'.get_field('tickets_link', 2).'" class="tickets" target="_blank">Tickets</a></div>';
 		}
 		$count++;
-		endwhile; endif; */} } ?>
+		endwhile; endif; } } ?>
 	</div>
 </div>
 
